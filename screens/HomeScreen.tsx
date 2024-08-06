2e1af47e68103cf05f2139b8b51272d506ba1911
Fidele <jackie.fidele@laposte.net>
2024-08-06 14:42:46 +0200

    Convert file into ts ans tsx, APP>Type App, HOMESCREEN>add navigation.d.ts for navigation type

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -2,9 +2,16 @@ import { StyleSheet, View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import Picture from "../components/Home/Picture";
 import Footer from "../components/Home/Footer";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { RootStackParamList } from "../../navigation";
+
+type HomeScreenNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  'Home'
+>;
 
 export default function HomeScreen() {
-  const navigation = useNavigation();
+  const navigation = useNavigation<HomeScreenNavigationProp>();
   const handleClick = () => {
     navigation.navigate("Search");
   };
